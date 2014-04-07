c209d32aed99f88097ddeb9e02969b877bc7a08e
ShengMin Zhang <me@shengmin.me>
2014-04-07 02:31:52 -0400

    Add sublime-text

--- configure.dart
+++ configure.dart
@@ -6,7 +6,8 @@ import 'common.dart';
 void main() {
   var links = [
     new Linking('gitconfig', join(DIR_HOME, '.gitconfig')),
-    new Linking('pam-environment', join(DIR_HOME, '.pam_environment'))
+    new Linking('pam-environment', join(DIR_HOME, '.pam_environment')),
+    new Linking('sublime-text', join(DIR_HOME, '.config/sublime-text-2/Packages/User/Preferences.sublime-settings'))
   ];
 
   // Linking configuration files
@@ -17,6 +18,7 @@ void main() {
       new Link(link.link).delete();
     } else if (FileSystemEntity.isFileSync(link.link)) {
       // Back up the file
+      print('$YELLOW File ${link.link} exists$RESET');
       new File(link.link).renameSync(
         join(DIR_BACKUP, now + '-' + basename(link.link)));
     }
